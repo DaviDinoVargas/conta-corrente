64088497b046ae05bca47c531876feee2d2ec1af
davidinovargas <davidinovargas@gmail.com>
2025-04-02 16:06:23 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaBancaria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    
+    public class ContaBancaria
+    {
+        public int Numero { get; set; }
+        public decimal Saldo { get; private set; }
+        public decimal Limite { get; set; }
+        private string[] Movimentacoes = new string[100];
+        private int IndexMovimentacoes = 0;
+
+        public ContaBancaria(int numero, decimal saldoInicial, decimal limite)
+        {
+            Numero = numero;
+            Saldo = saldoInicial;
+            Limite = limite;
+        
+        }
+
+
+
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta1 = new ContaBancaria(12, 1000, 0);
+            ContaBancaria conta2 = new ContaBancaria(13, 300, 0);
+
+            while (true)
+            {
+                Console.WriteLine("Escolha uma operação:");
+                Console.WriteLine("1 - Saque");
+                Console.WriteLine("2 - Depósito");
+                Console.WriteLine("3 - Consulta de Saldo");
+                Console.WriteLine("4 - Emissão de Extrato");
+                Console.WriteLine("5 - Transferência entre Contas");
+                Console.WriteLine("0 - Sair");
+
+                string escolha = Console.ReadLine();
+                decimal valor;
+
+                switch (escolha)
+                {
+                    case "1":
+                        Console.Write("Informe o valor do saque: ");
+                        valor = Convert.ToDecimal(Console.ReadLine());
+
+                        break;
+                    case "2":
+                        Console.Write("Informe o valor do depósito: ");
+                        valor = Convert.ToDecimal(Console.ReadLine());
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+                        Console.Write("Informe o valor da transferência: ");
+                        valor = Convert.ToDecimal(Console.ReadLine());
+
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+        }
+    }
+}
